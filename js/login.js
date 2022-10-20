3a9f22c875f77045c869af320985daa129928261
Luís Henrique <109689914+Luis-Henrique-Lima@users.noreply.github.com>
2022-10-20 19:15:40 -0300

    joguei o js certo e coloquei comentários

--- js/login.js
+++ js/login.js
@@ -1,18 +1,22 @@
+// função
 function Login() {
-  var done=0;
-  var usuario = document.getElementsByName('usuario')[0].value;
-  usuario=usuario.toLowerCase();
-  var senha= document.getElementsByName('senha')[0].value;
-  senha=senha.toLowerCase();
-  if (usuario=="zr" && senha=="zr") {  
-    swal({
-  title: "Sucesso",
-  text: "Login Efetuado",
-  icon: "success",
-  button: false,
-  });
-   
-    setTimeout(function(){window.location="index.html"},1250);
+  // variáveis
+    var done=0;
+    var usuario = document.getElementsByName('usuario')[0].value;
+    usuario=usuario.toLowerCase();
+    var senha= document.getElementsByName('senha')[0].value;
+    senha=senha.toLowerCase();
+      //alert se  der certo
+    if (usuario=="z" && senha=="z") {  
+      swal({
+    title: "Sucesso",
+    text: "Login Efetuado",
+    icon: "success",
+    button: false,
+    });
+          // função para ir para pagina inicial
+      setTimeout(function(){window.location="index.html"},1250);
+     }  // alerte se da erro
+    else if (done==0) { swal("Erro de Login!", "Usuári ou Senha incorreta", "error"); }
   }
-  else if (done==0) { swal("Erro de Login!", "Usuário ou Senha incorreta", "error"); }
-}
+  
