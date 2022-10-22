502312372ab844452931dcfae5559a3333b7236a
caaiopereira <caaiopereira9@gmail.com>
2022-10-22 18:03:03 -0300

    Delete info.js

--- js/info.js
+++
@@ -1,7 +0,0 @@
-let user = document.getElementById('usuario');
-let pass = document.getElementById('password');
-  
-function Salvar(){
-    localStorage.usuario = user.value;
-    localStorage.password = pass.value;
-}
