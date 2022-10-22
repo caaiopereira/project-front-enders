932f24315b07202a868321798ecb590733d91552
caaiopereira <caaiopereira9@gmail.com>
2022-10-22 16:41:06 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,4 +1,27 @@
 // função
+  function Login() {
+    let done = 0;
+    let user = document.getElementsByName('login')[0].value;
+    let pass = document.getElementsByName('senha')[0].value;
+
+    if (user == localStorage.usuario && pass == localStorage.password) {
+      swal({
+    title: "Sucesso",
+    text: "Login Efetuado, Você será direcionado a página inicial!",
+    icon: "success",
+    button: false,
+    });
+          // função para ir para pagina inicial
+      setTimeout(function(){window.location="index.html"},2100);
+     }  // alerte se da erro
+    else if (done==0) { swal("Error!", "Usuário ou Senha Invalidos", "error"); }
+    }
+
+console.log(localStorage.usuario);
+console.log(localStorage.password);
+
+
+/* // função
 function Login() {
   // variáveis
     let done=0;
@@ -19,4 +42,4 @@ function Login() {
      }  // alerte se da erro
     else if (done==0) { swal("Error!", "Usuário ou Senha Invalidos", "error"); }
   }
-
+  */
